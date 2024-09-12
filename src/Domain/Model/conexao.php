6440eef72064f5df45d295b8ab262f677026131c
lu9932323258@gmail.com <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-09-12 15:42:20 -0300

    criada a tabela

--- src/Domain/Model/conexao.php
+++ src/Domain/Model/conexao.php
@@ -6,3 +6,6 @@ $caminhoBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:'.$caminhoBanco);
 
 echo 'conectei';
+
+//cria a tabela atraves do sqlite
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, nome TEXT, birth_date TEXT);');
